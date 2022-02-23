1d0fc5c15be47aa8339119ea89cf7886c521a36a
marycatz <mary@catzmedia.com>
2022-02-24 02:46:18 +0800

    Add files via upload

--- resources/views/register-sme.blade.php
+++ resources/views/register-sme.blade.php
@@ -18,29 +18,27 @@
                         <div class="card overflow-hidden">
                             <div class="bg-primary bg-soft">
                                 <div class="row">
-                                    <div class="col-7">
+                                    <div class="col-12">
                                         <div class="text-primary p-4">
                                             
                                             <h5 class="text-primary">SME Registration</h5>
                                             <p>SME registration is FREE for Lifetime. Some instructions here (TBA)</p>
                                         </div>
                                     </div>
-                                    <div class="col-5 align-self-end">
+                                    <!-- div class="col-5 align-self-end">
                                         <img src="{{ URL::asset('/assets/images/profile-img.png') }}" alt=""
                                             class="img-fluid">
-                                    </div>
+                                    </div -->
                                 </div>
                             </div>
                             <div class="card-body pt-0">
                                 <div>
-                                    <a href="index">
                                         <div class="avatar-md profile-user-wid mb-4">
                                             <span class="avatar-title rounded-circle bg-light">
-                                                <img src="{{ URL::asset('/assets/images/logo.svg') }}" alt=""
-                                                    class="rounded-circle" height="34">
+                                                <img src="{{ asset ('assets/images2/home2/sme-register.png') }}" alt=""
+                                                    class="rounded-circle" height="100">
                                             </span>
                                         </div>
-                                    </a>
                                 </div>
                                 <div class="p-2">
                                     
@@ -80,6 +78,7 @@
                                                 <input type="file" class="form-control @error('ssmfile') is-invalid @enderror" id="inputGroupFile02" name="ssmfile" autofocus required>
                                                 <label class="input-group-text" for="inputGroupFile02">Upload</label>
                                             </div>
+                                            <i style="font-size:12px">Suruhanjaya Syarikat Malaysia (Companies Commission of Malaysia) - Sijil Pendaftaran Syarikat <b>Form 9</b> or <b>Section 17</b></i> 
                                             @error('ssmfile')
                                                 <span class="invalid-feedback" role="alert">
                                                     <strong>{{ $message }}</strong>
@@ -145,7 +144,7 @@
                                             </select>
                                         </div>
                                         <br>
-                                        <h4>Contact Person (Primary Contact)</h4>
+                                        <h4>Owner Information</h4>
                                         <br>
                                         <div class="mb-3">
                                             <label for="username" class="form-label">Full Name</label>
@@ -158,6 +157,18 @@
                                                 </span>
                                             @enderror
                                         </div>
+
+                                        <div class="mb-3">
+                                            <label for="username" class="form-label">IC / Passport Number</label>
+                                            <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                            value="{{ old('name') }}" id="username" name="name" autofocus required
+                                                placeholder="Enter ic/passport number">
+                                            @error('name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>                                        
 
                                         <div class="mb-3">
                                             <label for="position" class="form-label">Role/Position in Company</label>
