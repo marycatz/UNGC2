072e0fa03bc3c7284e747bf3a4360e436588bded
marycatz <mary@catzmedia.com>
2022-02-24 19:41:00 +0800

    Add files via upload

--- resources/views/components/breadcrumb.blade.php
+++ resources/views/components/breadcrumb.blade.php
@@ -1,5 +1,11 @@
 <!-- start page title -->
-<div class="row">
+
+<link href="{{ URL::asset('/assets/css/main.css') }}" rel="stylesheet" type="text/css" />
+
+       <section id="page-breadcrumb">
+            <div class="vertical-center sun">
+                 <div class="container">
+                    <div class="row">
     <div class="col-12">
         <div class="page-title-box d-sm-flex align-items-center justify-content-between">
             <h4 class="mb-sm-0 font-size-18">{{ $title }}</h4>
@@ -7,13 +13,11 @@
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
                     <li class="breadcrumb-item"><a href="javascript: void(0);">{{ $li_1 }}</a></li>
-                    @if(isset($title))
-                        <li class="breadcrumb-item active">{{ $title }}</li>
-                    @endif
                 </ol>
             </div>
-
         </div>
     </div>
-</div>
+    </div>
+            </div>
+       </section><br />
 <!-- end page title -->
